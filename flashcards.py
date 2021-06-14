053a47f6a23eb59e753204cca6d74b45ed1ef727
Ankit Chobdar <ankitchobdar@gmail.com>
2021-06-14 17:26:37 +0530

    cards app with css

--- flashcards.py
+++ flashcards.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, abort, jsonify
-from model import db
+from flask import Flask, render_template, abort, jsonify, request, redirect, url_for
+from model import db, save_db
 
 app = Flask(__name__)
 
@@ -17,6 +17,34 @@ def card_view(index):
     try:
         card = db[index]
         return render_template("card.html", card=card, index=index, max_index=3)
+    except IndexError:
+        abort(404)
+
+
+@app.route('/card/add_card', methods=["GET", "POST"])
+def add_card():
+    if request.method == "POST":
+        # form has been submitted process user data
+        card = {
+            "question": request.form["question"],
+            "answer": request.form["answer"]
+            }
+        db.append(card)
+        save_db()
+        return redirect(url_for('card_view', index=len(db)-1))
+    else:
+        return render_template('add_card.html')
+
+
+@app.route('/card/remove_card/<int:index>', methods=["GET", "POST"])
+def remove_card(index):
+    try:
+        if request.method == "POST":
+            del db[index]
+            save_db()
+            return redirect(url_for('welcome', cards=db))
+        else:
+            return render_template('remove_card.html', card=db[index])
     except IndexError:
         abort(404)
 
